a65b26182e1d4678103c643eafe96a6de7b1a68e
erlfas <erlfas@users.noreply.github.com>
2019-06-02 17:11:04 +0200

    good

--- src/main/java/no/fasmer/pastebin/PastebinPastesApplication.java
+++ src/main/java/no/fasmer/pastebin/PastebinPastesApplication.java
@@ -1,4 +1,4 @@
-package no.fasmer.pastebin.pastes;
+package no.fasmer.pastebin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
